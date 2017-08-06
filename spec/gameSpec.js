a221916d837bc8c866d1b949bbfc009b562b0a32
chuk-chuk <yulia.v.litvin@gmail.com>
2017-08-06 14:53:19 +0100

    can roll a spare

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -7,19 +7,28 @@ describe ('Bowling game', function(){
 
   it('can roll a gutter game', function(){
 
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+    rollMany(0, 20);
     expect(game.score()).toEqual(0);
   });
 
   it('can roll all ones in every roll', function(){
 
-    for(var i = 0; i < 20; i++) {
-      game.roll(1);
-    }
+    rollMany(1, 20);
     expect(game.score()).toEqual(20);
   });
 
-  
+  it('can roll a spare', function(){
+    game.roll(5);
+    game.roll(5);
+    game.roll(2);
+    rollMany(0, 17);
+    expect(game.score()).toEqual(14);
+  });
+
+  var rollMany = function(pins, rolls){
+    for (var i = 0; i < rolls; i++) {
+      game.roll(pins);
+    }
+  };
+
 });
--- src/game.js
+++ src/game.js
@@ -8,8 +8,29 @@ BowlingGame.prototype.roll = function(pins){
 
 BowlingGame.prototype.score = function(){
   var result = 0;
-  for (i = 0; i < 20; i++) {
-    result += this.rolls[i];
+  var rollIndex = 0; //[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+  var game = this;
+
+  for (frameIndex = 0; frameIndex < 10; frameIndex++) {
+    if (isSpare()){
+      result += getSpareScore();
+    } else {
+    result += getNormalScore();
+    }
+    rollIndex += 2;
   }
   return result;
+
+  function isSpare(){
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] == 10;
+  }
+
+  function getSpareScore(){
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+  }
+
+  function getNormalScore(){
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1];
+  }
+
 };
